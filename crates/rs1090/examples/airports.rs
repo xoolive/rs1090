7b90f20119065bd2ce9a0ef14654df9e4d357ba4
Xavier Olive <git@xoolive.org>
2024-03-22 14:59:38 +0100

    support for airports in reference latlon

---
+++ crates/rs1090/examples/airports.rs
@@ -0,0 +1,23 @@
+use regex::Regex;
+use rs1090::airports::AIRPORTS;
+
+pub fn main() {
+    let args: Vec<Regex> = std::env::args()
+        .skip(1)
+        .map(|a| Regex::new(&format!("(?i){}", &a)).unwrap())
+        .collect();
+
+    'airport: for airport in AIRPORTS.iter() {
+        for arg in &args {
+            if !arg.is_match(&airport.country)
+                & !arg.is_match(&airport.icao)
+                & !arg.is_match(&airport.iata)
+                & !arg.is_match(&airport.city)
+                & !arg.is_match(&airport.name)
+            {
+                continue 'airport;
+            }
+        }
+        println!("{}", airport);
+    }
+}
