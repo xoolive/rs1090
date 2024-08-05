2e7ce482b238a336fe2494b90205bf1ea2a25c2c
Xiaogang <maple.hl@gmail.com>
2024-08-05 10:27:21 +0800

    flake support

---
+++ flake.nix
@@ -0,0 +1,103 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ self, fenix, crane, flake-parts, advisory-db, ... }:
+    flake-parts.lib.mkFlake { inherit self inputs; } ({ withSystem, ... }: {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { lib, config, self', inputs', pkgs, system, ... }:
+        let
+          # rustToolchain = fenix.packages.${system}.fromToolchainFile {
+          #   file = ./rust-toolchain.toml;
+          #   sha256 = "sha256-Ngiz76YP4HTY75GGdH2P+APE/DEIx2R/Dn+BwwOyzZU=";
+          # };
+          rustToolchain = fenix.packages.${system}.stable.toolchain;
+          craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+          version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).workspace.package.version;
+
+          commonArgs = {
+            src = craneLib.cleanCargoSource ./.;
+            pname = "rs1090";
+            version = version;
+            nativeBuildInputs = with pkgs; [ pkg-config openssl clang mold python3 bzip2];
+            buildInputs = [ ] ++ lib.optionals pkgs.stdenv.isDarwin [
+              pkgs.libiconv
+            ];
+
+            RUSTFLAGS="-C linker=clang -C link-arg=-fuse-ld=${pkgs.mold}/bin/mold";
+          };
+
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            inputsFrom = builtins.attrValues self.checks;
+            buildInputs = [ rustToolchain pkgs.pkg-config pkgs.openssl ];
+          };
+
+          packages =
+            {
+              default =  craneLib.buildPackage (commonArgs // {  # TODO  this does not build for now
+                pname = "jet1090";
+                cargoExtraFlags = "-p jet1090";
+                meta.mainProgram = "jet1090";
+                inherit cargoArtifacts;
+              });
+
+              # docs = pkgs.callPackage ./docs {};
+            };
+
+          checks =
+            {
+              fmt = craneLib.cargoFmt (commonArgs);
+              audit = craneLib.cargoAudit (commonArgs // { inherit advisory-db; });
+              rustdoc = craneLib.cargoDoc (commonArgs // { inherit cargoArtifacts; });
+
+              clippy-check = craneLib.cargoClippy (commonArgs // {
+                inherit cargoArtifacts;
+                cargoClippyExtraArgs = "--all-features -- --deny warnings";
+              });
+
+              test-check = craneLib.cargoNextest (commonArgs // {
+                inherit cargoArtifacts;
+                partitions = 1;
+                partitionType = "count";
+              });
+            }
+            # build packages as part of the checks
+            // (lib.mapAttrs' (key: value: lib.nameValuePair (key + "-package") value) self'.packages);
+
+          formatter = pkgs.nixpkgs-fmt;
+        };
+    });
+
+  nixConfig = {
+    extra-trusted-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+  };
+}
