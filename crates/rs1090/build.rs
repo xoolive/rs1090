b54deb345e72143071d97e64550c9f11034b070b
Xavier Olive <git@xoolive.org>
2024-11-17 23:55:05 +0100

    should fix github actions

--- crates/rs1090/build.rs
+++ crates/rs1090/build.rs
@@ -1,6 +1,6 @@
 #[cfg(feature = "sero")]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/SeroAPI.proto")?;
+    tonic_build::compile_protos("proto/SeRoAPI.proto")?;
     Ok(())
 }
 #[cfg(not(feature = "sero"))]
