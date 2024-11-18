81c410c08890d19a7a9ab7f062ea278d7b104859
Xavier Olive <git@xoolive.org>
2024-11-18 23:17:50 +0100

    auto download+cache the ca.crt file

--- crates/rs1090/src/source/sero.rs
+++ crates/rs1090/src/source/sero.rs
@@ -3,30 +3,59 @@ mod api {
     tonic::include_proto!("serosystems.proto.v3.backend.api");
 }
 
+use std::path::PathBuf;
+
 use api::{
     se_ro_api_client::SeRoApiClient, ModeSDownlinkFrame,
     ModeSDownlinkFramesRequest, SensorInfoRequest, SensorInfoResponse,
 };
 use serde::Deserialize;
 use serde::Serialize;
-use tokio::fs;
+use tokio::fs::{self, File};
+use tokio::io::AsyncWriteExt;
 use tonic::{
     transport::{Certificate, Channel, ClientTlsConfig},
     Streaming,
 };
+use tracing::info;
+
+type Result<T> =
+    std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct SeroClient {
     pub token: String,
-    pub ca_cert: String,
+}
+
+async fn download_file(url: &str, destination: &PathBuf) -> Result<()> {
+    let response = reqwest::get(url).await?.bytes().await?;
+    let mut file = File::create(destination).await?;
+    file.write_all(&response).await?;
+    Ok(())
 }
 
 impl SeroClient {
-    pub async fn client(
-        self,
-    ) -> Result<SeRoApiClient<Channel>, Box<dyn std::error::Error>> {
+    pub async fn client(self) -> Result<SeRoApiClient<Channel>> {
+        let mut cache_path = dirs::cache_dir().unwrap_or_default();
+        cache_path.push("jet1090");
+        if !cache_path.exists() {
+            let msg =
+                format!("failed to create {:?}", cache_path.to_str().unwrap());
+            fs::create_dir_all(&cache_path).await.expect(&msg);
+        }
+
+        let ca_cert_url = "https://doc.sero-systems.de/api/_downloads/017ce4f89360621e0345c257b6136b21/ca.crt";
+        let ca_cert_file = "ca_sero.crt";
+
+        cache_path.push(ca_cert_file);
+        if !cache_path.exists() {
+            info!("Downloading sero certificate");
+            download_file(ca_cert_url, &cache_path).await?;
+        }
+
         // Load the CA certificate
-        let ca_cert = fs::read(self.ca_cert).await?;
+        info!("Loading sero certificate: {:?}", cache_path);
+        let ca_cert = fs::read(cache_path).await?;
         let ca_cert = Certificate::from_pem(ca_cert);
 
         // Configure TLS
@@ -41,39 +70,31 @@ impl SeroClient {
         Ok(SeRoApiClient::new(channel))
     }
 
-    pub async fn info(self) -> Option<SensorInfoResponse> {
+    pub async fn info(self) -> Result<SensorInfoResponse> {
         let request = tonic::Request::new(SensorInfoRequest {
             token: self.token.clone(),
             sensors: vec![],
         });
-        if let Some(mut client) = self.client().await.ok() {
-            client
-                .get_sensor_info(request)
-                .await
-                .ok()
-                .map(|s| s.into_inner())
-        } else {
-            None
-        }
+        Ok(self
+            .client()
+            .await?
+            .get_sensor_info(request)
+            .await?
+            .into_inner())
     }
 
-    pub async fn rawstream(self) -> Option<Streaming<ModeSDownlinkFrame>> {
+    pub async fn rawstream(self) -> Result<Streaming<ModeSDownlinkFrame>> {
         let request = tonic::Request::new(ModeSDownlinkFramesRequest {
             token: self.token.clone(),
-            df_filter: vec![],
+            df_filter: vec![17, 18, 20, 21],
             sensor_filter: vec![],
             aircraft_filter: vec![],
         });
-        // This may look weird but causes error if we write if let Ok()
-        // This is due to dyn StdError not being thread-safe
-        if let Some(mut client) = self.client().await.ok() {
-            client
-                .get_mode_s_downlink_frames(request)
-                .await
-                .ok()
-                .map(|s| s.into_inner())
-        } else {
-            None
-        }
+        Ok(self
+            .client()
+            .await?
+            .get_mode_s_downlink_frames(request)
+            .await?
+            .into_inner())
     }
 }
